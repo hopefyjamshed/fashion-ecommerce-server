7451df4371f87cd40e00daef1edccb5c412abb88
hopefyjamshed <103117529+hopefyjamshed@users.noreply.github.com>
2023-06-11 18:24:52 +0600

    upload a product added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,19 +16,31 @@ app.get('/', (req, res) => {
 
 // mongodb connection
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jle6tre.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
     try {
-        const ProuductCollections = client.db('FashionSite').collection('ProductCollections')
+        const ProuductCollection = client.db('FashionSite').collection('ProductCollections')
+        const uploadCollections = client.db("FashionSite").collection("uploadCollections")
 
         app.get('/products', async (req, res) => {
             const query = {}
-            const result = await ProuductCollections.find(query).toArray()
+            const result = await ProuductCollection.find(query).toArray()
             res.send(result)
         })
 
+        app.post('/uploadProduct', async (req, res) => {
+            const upload = req.body
+            const result = await uploadCollections.insertOne(upload)
+            res.send(result)
+
+        })
     }
     finally {
 
